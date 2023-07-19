4d60fe7d7a21dd1a5b6b49c4add86d001111dc74
Simon <sem.bortnik@googlemail.com>
2023-07-19 20:41:38 +0200

    Added working example of simplex

--- sv/index.py
+++ sv/index.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from scipy.optimize import linprog
 
 app = Flask(__name__)
 
@@ -6,4 +7,20 @@ test = "qweqweqwe"
 
 @app.route("/")
 def hello_world():
-    return f'<p>{test}</p>'
+    c = [-3, -1, -3]
+
+    A = [[2, 1, 1], [1, 2, 3], [2, 2, 1]]
+    b = [2, 5, 6]
+
+    x0_bounds = (0, None)
+    x1_bounds = (0, None)
+    x2_bounds = (0, None)
+
+    res = linprog(c, A_ub=A, b_ub=b, bounds=[x0_bounds, x1_bounds, x2_bounds])
+
+    res.fun
+
+    res.x
+
+    res.message
+    return f'<p>{res.x}</p>'
