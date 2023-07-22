f9f671559b67344667200efae3fc0bb87df2162f
Simon <sem.bortnik@googlemail.com>
2023-07-22 17:47:49 +0200

    Added rudimentary data preparation and updated data

--- sv/index.py
+++ sv/index.py
@@ -7,24 +7,35 @@ from flask import current_app
 app = Flask(__name__)
 
 def prepareData(drivers):
-    drivers = drivers.dropna(axis=1, how='all')
-    drivers['total'] = drivers['r0'] + drivers['r1'] + drivers['r2'] + drivers['r3'] + drivers['r4'] + drivers['r5'] + drivers['r6'] + drivers['r7'] + drivers['r8'] + drivers['r9'] + drivers['r10']
-    return drivers
+    drivers.loc[:,'total'] = 0
+    
+    # Prepare calculation dataframe
+    driversPoints = drivers.copy().drop(['cost'], axis=1).dropna(axis=1, how='all').transpose()
+    
+    # Prepare overview df
+    driversOverview = drivers.copy().dropna(axis=1, how='all')
 
+    print(driversOverview.head(3), file=sys.stderr)
+    print(driversPoints.head(3), file=sys.stderr)
+    return driversOverview, driversPoints
+
+# Takes series of total points scored and returns them multiplied by -1 as array
+def getMinimizeArr(totals):
+    return totals.apply(lambda x : x * -1).array
 drivers = pd.read_excel('./data/test.xlsx', index_col=0, header=0) # TODO: Make path OS agnostic
-drivers['total'] = drivers['r1'] * 10
-app.drivers = prepareData(drivers)
+app.driversOverview, app.driversPoints = prepareData(drivers)
 
 
 @app.route("/drivers")
 def drivers():
-    print(current_app.drivers.head(5), file=sys.stderr)
-    #print(current_app.drivers.dtypes, file=sys.stderr)
+    print(app.driversPoints.mean(), file=sys.stderr)
     return "qweqwe"
 
 @app.route("/")
 def hello_world():
-    c = [-3, -1, -3]
+    c = getMinimizeArr(app.driversPoints.sum())
+    print(c, file=sys.stderr)
+    print(app.driversOverview['cost'].values, file=sys.stderr)
 
     A = [[2, 1, 1], [1, 2, 3], [2, 2, 1]]
     b = [2, 5, 6]
@@ -33,11 +44,11 @@ def hello_world():
     x1_bounds = (0, None)
     x2_bounds = (0, None)
 
-    res = linprog(c, A_ub=A, b_ub=b, bounds=[x0_bounds, x1_bounds, x2_bounds])
+    #res = linprog(c, A_ub=A, b_ub=b, bounds=[x0_bounds, x1_bounds, x2_bounds])
 
-    res.fun
+    #res.fun
 
-    res.x
+    #res.x
 
-    res.message
-    return f'<p>{res.x}</p>'
+    #res.message
+    return f'<p></p>'
