2937d87d4de005bfdf7c2bd1e42c552c6f43ace6
Simon <sem.bortnik@googlemail.com>
2023-07-25 22:31:16 +0200

    Updated dataset, optimization now works

--- sv/index.py
+++ sv/index.py
@@ -6,49 +6,101 @@ from flask import current_app
 
 app = Flask(__name__)
 
-def prepareData(drivers):
-    drivers.loc[:,'total'] = 0
-    
+def prepareData(drivers, constructors):
     # Prepare calculation dataframe
-    driversPoints = drivers.copy().drop(['cost'], axis=1).dropna(axis=1, how='all').transpose()
-    
+    driversPoints = drivers.copy().drop(['cost'], axis=1).dropna(axis=1, how='all').transpose() # TODO: Drop race woth no starts
+    constructorsPoints = constructors.copy().drop(['cost'], axis=1).dropna(axis=1, how='all').transpose() # TODO: Drop race woth no starts
+
     # Prepare overview df
     driversOverview = drivers.copy().dropna(axis=1, how='all')
+    constructorsOverview = constructors.copy().dropna(axis=1, how='all')
 
-    print(driversOverview.head(3), file=sys.stderr)
-    print(driversPoints.head(3), file=sys.stderr)
-    return driversOverview, driversPoints
+    return driversOverview, driversPoints, constructorsOverview, constructorsPoints
+
+drivers = pd.read_excel('./data/test.xlsx', index_col=0, header=0) # TODO: Make path OS agnostic
+constructors = pd.read_excel('./data/test.xlsx', index_col=0, header=0, sheet_name=1) # TODO: Make path OS agnostic
+app.driversOverview, app.driversPoints, app.constructorsOverview, app.constructorsPoints = prepareData(drivers, constructors)
 
 # Takes series of total points scored and returns them multiplied by -1 as array
 def getMinimizeArr(totals):
+    totals = pd.Series(totals)
     return totals.apply(lambda x : x * -1).array
-drivers = pd.read_excel('./data/test.xlsx', index_col=0, header=0) # TODO: Make path OS agnostic
-app.driversOverview, app.driversPoints = prepareData(drivers)
+
+def getCostArray():
+    return app.driversOverview['cost'].to_numpy().tolist() + app.constructorsOverview['cost'].to_numpy().tolist()
+
+def getBoundsArray():
+    bounds = []
+    for x in range(0, 30):
+        bounds.append((0, 1))
+    return bounds
+
+def getDriversDecisionArray(decision):
+    drivers = []
+    for x in range(0, 20):
+        drivers.append(decision)
+    return drivers
+
+def getConstructorsDecisionArray(decision): 
+    constructors = []
+    for x in range(0, 10):
+        constructors.append(decision)
+    return constructors
+
+def getDecisionArray():
+    return getDriversDecisionArray(1) + getConstructorsDecisionArray(1)
+
+def getResultString(results):
+    namesArray = []
+
+    # Handle drivers
+    driversResults = results[0:20]
+    for x in range(len(driversResults)):
+        if(driversResults[x] == 1):
+            namesArray.append(app.driversOverview.index[x])
+    returnString = 'Optimal driver picks are:'
+    for i in range(0, len(namesArray)):
+        returnString +=  ' ' + namesArray[i]
+        if i < 5:
+            returnString += ','
+
+    # Handle constructors
+    namesArray = []
+    returnString += ' ||| '
+    constructorsResults = results[20:29]
+    for x in range(len(constructorsResults)):
+        if(constructorsResults[x] == 1):
+            namesArray.append(app.constructorsOverview.index[x])
+    returnString += 'Optimal constructor picks are:'
+    for i in range(0, len(namesArray)):
+        returnString +=  ' ' + namesArray[i]
+        if i < 2:
+            returnString += ','
+    
+    return returnString
+
+def CostString():
+    return ''
 
 
 @app.route("/drivers")
 def drivers():
-    print(app.driversPoints.mean(), file=sys.stderr)
     return "qweqwe"
 
 @app.route("/")
 def hello_world():
-    c = getMinimizeArr(app.driversPoints.sum())
-    print(c, file=sys.stderr)
-    print(app.driversOverview['cost'].values, file=sys.stderr)
+    c = getMinimizeArr(app.driversPoints.sum().to_list() + app.constructorsPoints.sum().to_list())
 
-    A = [[2, 1, 1], [1, 2, 3], [2, 2, 1]]
-    b = [2, 5, 6]
+    # Lesser than constraints
+    A_ub = [getCostArray()]
+    b_ub = [100]
 
-    x0_bounds = (0, None)
-    x1_bounds = (0, None)
-    x2_bounds = (0, None)
+    #Equality contraints
+    A_eq = [getDriversDecisionArray(1) + getConstructorsDecisionArray(0), getDriversDecisionArray(0) + getConstructorsDecisionArray(1)]
+    b_eq = [5, 2]
 
-    #res = linprog(c, A_ub=A, b_ub=b, bounds=[x0_bounds, x1_bounds, x2_bounds])
+    res = linprog(c, A_ub=A_ub, b_ub=b_ub, A_eq=A_eq, b_eq=b_eq, bounds=getBoundsArray(), integrality=1)
 
-    #res.fun
-
-    #res.x
-
-    #res.message
-    return f'<p></p>'
+    print(res.x)
+    return f'<p>{getResultString(res.x)}</p>'
+    #return f'<p>Stand by</p>'
